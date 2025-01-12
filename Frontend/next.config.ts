bab5ecba89d8064c7d59c4ffbbb3e118e42b7a3e
Silas Schnurr <silas@schnurr.one>
2025-01-12 16:26:31 +0100

    aadded asset prefix again

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath:
     process.env.NODE_ENV === "production" ? "/Template_Next" : undefined,
-
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/Template_Next/" : undefined,
   output: "export",
   reactStrictMode: true,
 };
