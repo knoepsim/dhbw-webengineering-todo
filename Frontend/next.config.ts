14650e750a519b99fb6102a60e33b81096652ae0
Silas Schnurr <silas@schnurr.one>
2025-01-13 19:45:04 +0100

    added nojekyll file

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath:
     process.env.NODE_ENV === "production" ? "/Template_Next" : undefined,
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/Template_Next/" : undefined,
+
   output: "export",
   reactStrictMode: true,
 };
