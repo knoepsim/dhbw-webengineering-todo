a712fbf4a0de07177337cb6cabfb5731fcacdf71
Silas Schnurr <silas@schnurr.one>
2025-01-13 19:47:28 +0100

    updated asset prefix

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath:
     process.env.NODE_ENV === "production" ? "/Template_Next" : undefined,
-
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://dhbw-ka-webengineering.github.io/Template_Next/"
+      : undefined,
   output: "export",
   reactStrictMode: true,
 };
