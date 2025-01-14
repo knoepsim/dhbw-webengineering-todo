d11621b15c5159a62a1fd7498465065051f95bb9
Lukas Panni <lukas.panni@outlook.de>
2025-01-14 16:30:25 +0100

    Fix gh pages deployment

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
+type NodeEnvType = { BASE_PATH?: "gh-pages" | string };
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends NodeEnvType {}
+  }
+}
+
 const nextConfig: NextConfig = {
-  basePath:
-    process.env.NODE_ENV === "production" ? "/Template_Next" : undefined,
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/Template_Next/" : undefined,
+  basePath: process.env.BASE_PATH,
   output: "export",
   reactStrictMode: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
